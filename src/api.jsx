bc1c78915c1b7b88adba79bdd6b59a156f885e51
Guilherme <guilhermehenrike0412@gmail.com>
2026-08-03 16:54:43 -0300

    feat: base do projeto

---
+++ src/api.jsx
@@ -0,0 +1,34 @@
+import axios from 'axios'
+
+export const api = axios.create({
+    baseURL: 'http://localhost:8080/api',
+    withCredentials: true
+})
+
+    // AUTH
+
+export const signIn = (data) => {
+    return api.post(`/signIn`, data)
+}
+
+export const signUp = (data) => {
+    return api.post(`/signUp`, data)
+}
+
+    // ALBUM
+
+export const allAlbum = () => {
+    return api.get(`/album`)
+}
+
+export const createAlbum = (data) => {
+    return api.post(`/album`, data)
+}
+
+export const updateAlbum = (id, data) => {
+    return api.put(`/album/${id}`, data)
+}
+
+export const deleteAlbum = (id) => {
+    return api.delete(`/album/${id}`)
+}
